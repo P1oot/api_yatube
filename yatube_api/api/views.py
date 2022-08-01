4f1951d6f937d156c0f57eae769ae64a5ba11818
Даниил Бердинских <dan.berdinskikh@mail.ru>
2022-08-02 02:05:42 +0300

    Fix PEP8 warning

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -10,8 +10,8 @@ class IsOwner(permissions.BasePermission):
         return request.user.is_authenticated
 
     def has_object_permission(self, request, view, obj):
-        if (request.method in permissions.SAFE_METHODS or
-                request.method == 'POST'):
+        if (request.method in permissions.SAFE_METHODS
+                or request.method == 'POST'):
             return True
         return obj.author == request.user
 
